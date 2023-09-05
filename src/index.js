19fa2b40663c29e9cc3d2d7e2b5c54ea339b44ff
Bolphunga <aymendj@yahoo.com>
2023-09-06 01:53:19 +0200

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form")
+    inputForm.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        const input = document.querySelector("input#searchByID")
+        console.log("Submitted:" + e)
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(r=>r.json())
+        .then((json)=>{
+            console.log(json)
+            const title = document.querySelector("h4")
+            const summary = document.querySelector("p")
+            title.textContent = json.title
+            summary.textContent = json.summary
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
